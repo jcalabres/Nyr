19c8af0059a2128cbab2af5171b4aaab14e96b68
Ricardo Loura <ricardoloura@gmail.com>
2020-02-26 20:06:02 +0000

    Correct Main

--- Main.py
+++ Main.py
@@ -2,6 +2,5 @@ from Smali.Dex import *
 from Smali.Analyzer import *
 
 dex = Dex("classes.dex")
-#print(dex.getClassByName("Ls/h/e/l/l/S;"))
 print(dex.getClass(791))
 print(Analyzer.overview(dex))
